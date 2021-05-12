c22b4bba02546355d1806ee7e1ff58a63ed039f7
GoodAlex223 <alexminak32@gmail.com>
2021-05-12 18:33:53 +0300

    Update

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-# DEBUG = False
+# DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = False
 
 ALLOWED_HOSTS = ['tranquil-springs-25369.herokuapp.com', '127.0.0.1']
 
